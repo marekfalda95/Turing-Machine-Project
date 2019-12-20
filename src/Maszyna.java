290cd4091303c01e5853da45c95350d3fd472a82
marek <marekfalda95@gmail.com>
2019-12-20 14:55:44 +0100

    Typ taśmy zmieniony na LinkedList.

--- src/Maszyna.java
+++ src/Maszyna.java
@@ -1,14 +1,19 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Maszyna {
 	private static final int dlugoscTasmy = 32;
 	List<Character> tasma;
 	
-	public Maszyna() {
-		tasma = new ArrayList<Character>();
+	void rozszerzTasme(LinkedList<Character> tasma) {
+		for(int i = 0; i < dlugoscTasmy; ++i) {
+			tasma.add('#');
+		}
 	}
-
+	
+	public Maszyna() {
+		tasma = new LinkedList<Character>();
+	}
 	
 }
 
