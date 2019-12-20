6f63f7c60929804f432606db58a7db29c91c4aaf
marek <marekfalda95@gmail.com>
2019-12-20 14:37:28 +0100

    Initial commit.

---
+++ src/Maszyna.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Maszyna {
+	private static final int dlugoscTasmy = 32;
+	List<Character> tasma;
+	
+	public Maszyna() {
+		tasma = new ArrayList<Character>();
+	}
+
+	
+}
+
